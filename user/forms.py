a6bd83cfc6e0a071bbc930e5f35f269504b7f783
Shivam Kumar <Shivamkr.kr23@gmail.com>
2023-01-25 15:57:10 +0530

    project complete

--- user/forms.py
+++ user/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-class UserRegistrationForm(UserCreationForm):
+class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     phone_no = forms.CharField(max_length=20)
     first_name = forms.CharField(max_length=20)
